3b6a6373c26878633887055dd36522e08f2d5c15
Rama Milaneh <ramamilaneh@Ramas-MacBook-Air.local>
2016-10-24 10:22:14 -0400

    Done

--- Hospital/ViewController.swift
+++ Hospital/ViewController.swift
@@ -11,6 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       let jim = Principal(name: "Jim", vacationDays: 20)
+       print("Did Jim get the vacation \(jim.requestForVacation(15))")
+       print(jim.disciplineStudent(.severe))
+       print(jim.callParentDeliveringMessage(.light))
+        let wage = jim.wages()
+        print(wage)
+        let johann = Teacher(name: "Johann", vacationDays: 10)
+        print("Did Johann get the vacation \(jim.requestForVacation(22))")
+       print("Johann is teaching math\(johann.teachSubject(.math))")
         
         
         
